cf70525a9966a1e89f361a2c6e08f9298c1a0c26
admin <admin@MacBook-Pro-admin.local>
2020-09-12 15:02:50 +0200

    New version of find_roi

--- HybriD/Interface/find_roi.py
+++ HybriD/Interface/find_roi.py
@@ -8,6 +8,7 @@ def add_subparser(subparsers):
     roi.add_argument('input',  type=str)
     roi.add_argument('output', type=str)
     roi.add_argument('limit', type=int)
+    roi.add_argument('size', type=int)
 
 def action(args):
-    find_roi.run(input_file=args.input, output_file=args.output, limit_coverage=args.limit)
+    find_roi.run(input_file=args.input, output_file=args.output, limit_coverage=args.limit, minimum_size=args.size)
