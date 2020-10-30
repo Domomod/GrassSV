8ac1a7b26c7b808b061326b67e24cadda5fe121d
RenegadeWizard <ksychla5@gmail.com>
2020-10-30 15:21:24 +0100

    working script

--- HybriD/Region/Load/sam.py
+++ HybriD/Region/Load/sam.py
@@ -1,14 +1,15 @@
 class SamInstance:
-    def __init__(self, qname, flag, rname, pos, mapq, cigar, rnext, pnext, tlen, seq, qual, atr):
-        self.qname = qname  # if type(qname) == str
-        self.flag = int(flag)  # if type(flag) == int
-        self.rname = rname  # if type(rname) == str
-        self.pos = int(pos)  # if type(pos) == int
-        self.mapq = int(mapq)  # if type(mapq) == int
-        self.cigar = cigar  # if type(ciagr) == str
-        self.rnext = rnext  # if type(rnext) == str
-        self.pnext = int(pnext)  # if type(pnext) == int
-        self.tlen = int(tlen)  # if type(tlen) == int
-        self.seq = seq  # if type(seq) == str
-        self.qual = qual  # if type(qual) == str
-        self.atr = atr
+    def __init__(self, fileContent: [str]):
+        self.qname = fileContent[0]  # if type(qname) == str
+        self.flag = int(fileContent[1])  # if type(flag) == int
+        self.rname = fileContent[2]  # if type(rname) == str
+        self.pos = int(fileContent[3])  # if type(pos) == int
+        self.mapq = int(fileContent[4])  # if type(mapq) == int
+        self.cigar = fileContent[5]  # if type(ciagr) == str
+        self.rnext = fileContent[6]  # if type(rnext) == str
+        self.pnext = int(fileContent[7])  # if type(pnext) == int
+        self.tlen = int(fileContent[8])  # if type(tlen) == int
+        self.seq = fileContent[9]  # if type(seq) == str
+        self.qual = fileContent[10]  # if type(qual) == str
+        if len(fileContent) > 11:
+            self.atr = fileContent[11]
