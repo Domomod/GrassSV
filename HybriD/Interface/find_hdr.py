a07728a5255873c5c22a95ec1bcc81ddf8dcdbd4
RenegadeWizard <ksychla5@gmail.com>
2020-12-15 10:57:54 +0100

    add interface and more effective detection

--- HybriD/Interface/find_hdr.py
+++ HybriD/Interface/find_hdr.py
@@ -4,8 +4,10 @@ TEXT = 'find_hdr'
 
 
 def add_subparser(subparsers):
-    csv2bed = subparsers.add_parser(TEXT)
+    hdr = subparsers.add_parser(TEXT, help='finds regions of intrest based on mapping coverage')
+    hdr.add_argument('input', type=str)
+    hdr.add_argument('output', type=str)  # TODO: dodaj inne parametry
 
 
 def action(args):
-    find_hdr.find_hdr()
+    find_hdr.find_hdr(args.input, args.output)
--- HybriD/Region/DepthRegion.py
+++ HybriD/Region/DepthRegion.py
@@ -4,6 +4,30 @@ class DepthRegion:
         self.start = start
         self.end = end
 
+    def __sub__(self, other):
+        if self == other:
+            return 0
+        return self.start - other.end
+
+    def __gt__(self, other):
+        if self.chromosome != other.chromosome:
+            return False
+        if self == other:
+            return False
+        return self.start - other.end > 0
+
+    def __lt__(self, other):
+        if self.chromosome != other.chromosome:
+            return False
+        if self == other:
+            return False
+        return self.start - other.end < 0
+
+    def __eq__(self, other):
+        if self.chromosome != other.chromosome:
+            return False
+        return (other.start <= self.start <= other.end) or (other.start <= self.end <= other.end)
+
     def __repr__(self):
         return self.chromosome + "\t" + str(self.start) + '\t' + str(self.end) + '\n'
 
