616aa6622f63ceca8c63b2b8b414db127b627f0c
RenegadeWizard <ksychla5@gmail.com>
2020-12-17 17:57:18 +0100

    rewrite script

--- HybriD/Interface/fastq_regions.py
+++ HybriD/Interface/fastq_regions.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from ..Region.Load.fastq import FastqInstance
 from ..Region.Load.sam import SamInstance
 from ..Region.BioRegion import Region
@@ -8,37 +6,43 @@ TEXT = 'fastq_regions'
 
 
 def action(args):
-    in_sam = args.sam
-    in_depth = args.depth
-    out_fastq_1 = args.fastq1
-    out_fastq_2 = args.fastq2
+    depth = read_depth_file(args.depth)
 
+    with open(args.fastq1, 'w') as fastq1_file:
+        with open(args.fastq2, 'w') as fastq2_file:
+            first = None
+            for sam in read_sam_file(args.sam):
+                if not first:
+                    first = sam
+                    continue
+                second = sam
+                for currDepth in depth:
+                    if are_good(first, second, currDepth):
+                        fastq1_file.write(str(FastqInstance(first.qname, first.seq, first.qual, 1)))
+                        fastq2_file.write(str(FastqInstance(second.qname, second.seq, second.qual, 2)))
+                first = sam
+
+
+def read_depth_file(in_depth: str):
     with open(in_depth) as depth_file:
-        depth_file_content = [Region(i.split()[1], i.split()[2], i.split()[0]) for i in depth_file.readlines()]
+        return [Region(i.split()[1], i.split()[2], i.split()[0]) for i in depth_file.readlines()]
 
-    with open(in_sam) as sam_file:
-        with open(out_fastq_1, 'a+') as fastq1_file:
-            with open(out_fastq_2, 'a+') as fastq2_file:
-                sam_first = sam_file.readline()
-                while sam_first[0][0] == '@':
-                    sam_first = sam_file.readline()
-                sam_first = SamInstance(sam_first.split())
-                sam_second = sam_file.readline()
-                while sam_second != '':
-                    sam_second = SamInstance(sam_second.split())
-                    for currDepth in depth_file_content:
-                        same_chromosome = sam_first.rname == currDepth.ref
-                        are_paired = (sam_first.tlen == -sam_second.tlen)
-                        at_least_one_covered = (currDepth.start - len(
-                            sam_first.seq) << sam_first.pos << currDepth.end) or (currDepth.start - len(sam_second.seq) << sam_second.pos << currDepth.end)
-                        at_least_one_unmapped = sam_first.flag & 12 > 1
-                        if (
-                                same_chromosome and are_paired and at_least_one_covered) or at_least_one_unmapped:  # TODO typy
-                            fastq1_file.write(str(FastqInstance(sam_first.qname, sam_first.seq, sam_first.qual, 1)))
-                            fastq2_file.write(
-                                str(FastqInstance(sam_second.qname, sam_second.seq, sam_second.qual, 2)))
-                    sam_first = sam_second
-                    sam_second = sam_file.readline()
+
+def read_sam_file(sam_file_name: str):
+    for line in open(sam_file_name):
+        if line[0] == "@":
+            continue
+        yield SamInstance(line.split())
+
+
+def are_good(first, second, depth) -> bool:  # TODO: rename
+    same_chromosome = first.rname == depth.ref
+    are_paired = (first.tlen == -second.tlen)
+    at_least_one_covered = (depth.start - len(
+        first.seq) << first.pos << depth.end) or (depth.start - len(
+        second.seq) << second.pos << depth.end)
+    at_least_one_unmapped = first.flag & 12 > 1
+    return (same_chromosome and are_paired and at_least_one_covered) or at_least_one_unmapped
 
 
 def add_subparser(subparsers):
