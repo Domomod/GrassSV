84247f38f8158217e271e671d4ce42e81aa438ab
RenegadeWizard <ksychla5@gmail.com>
2020-12-27 11:18:11 +0100

    refactor

--- GrassSV/Grass4slurm/extract_reads.sh
+++ GrassSV/Grass4slurm/extract_reads.sh
@@ -13,9 +13,8 @@ coverage_dir=$mutation_dir/coverage_${max_depth}_$margin
 
 mkdir -p $coverage_dir
 printf "[+] find_roi ("$mutation_dir"):\t"
-HybriD.py find_roi $mutation_dir/depth.coverage $coverage_dir/regions_of_interest.bed $max_depth 1  $margin
+GrassSV.py find_roi $mutation_dir/depth.coverage $coverage_dir/regions_of_interest.bed $max_depth 1  $margin
 printf "Done\n[+] fastq_regions ("$mutation_dir"):\t"
-extract_reads.exe $mutation_dir/alignments.sam $coverage_dir/regions_of_interest.bed $coverage_dir/filtered_reads_C1.fastq $coverage_dir/filtered_reads_C2.fastq $coverage_dir 
-#GrassSV.py fastq_regions -f1 $coverage_dir/filtered_reads_C1.fastq -f2 $coverage_dir/filtered_reads_C2.fastq -s $mutation_dir/alignments.sam -d $coverage_dir/regions_of_interest.bed:
+#extract_reads.exe $mutation_dir/alignments.sam $coverage_dir/regions_of_interest.bed $coverage_dir/filtered_reads_C1.fastq $coverage_dir/filtered_reads_C2.fastq $coverage_dir
+GrassSV.py filter_reads -f1 $coverage_dir/filtered_reads_C1.fastq -f2 $coverage_dir/filtered_reads_C2.fastq -s $mutation_dir/alignments.sam -d $coverage_dir/regions_of_interest.bed:
 printf "Done\n"
-
