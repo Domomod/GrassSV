8abdf2ff93165dedb64adbcb5765e8388b5687b0
Dominik Witczak <domionato@gmail.com>
2021-11-01 12:08:30 +0100

    Add duplications detection to pipeline

--- GrassSV/GrassSlurm/Bash/whole_pipeline3.sh
+++ GrassSV/GrassSlurm/Bash/whole_pipeline3.sh
@@ -13,12 +13,14 @@ assembler=${2:-grasshopper}
 contigs=${3:-contigs.fsa}
 alignments=${4:-quast/contigs_reports/all_alignments_contigs.tsv}        
 
+GrassSV.py find_hdr ${data}/depth.coverage ${data}/duplications.bed.temp
 for coverage in 5 7 10 12
 do
 	for margin in 150 #250 350 450
 	do
 		run_quast.sh ${data}/coverage_${coverage}_${margin}/${assembler}/${contigs} quast
 		GrassSV.py find_sv ${data}/coverage_${coverage}_${margin}/${assembler}/${alignments} -o ${data}/coverage_${coverage}_${margin}/${assembler}/
+		cp ${data}/duplications.bed.temp  ${data}/coverage_${coverage}_${margin}/${assembler}/detectedSVs/duplications.bed
 	done
 done
-
+rm ${data}/duplications.bed.temp
