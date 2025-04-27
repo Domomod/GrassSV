22f740eb6dedd678218151aa1e5bbda5c591ca83
Domomod <domionato@gmail.com>
2025-04-27 07:58:57 +0200

    Introduce snakemake pipeline

--- GrassSV.py
+++ GrassSV.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # PYTHON_ARGCOMPLETE_OK
-import argcomplete
+#import argcomplete
 
 from GrassSV.Interface import main
 
 if __name__ == '__main__':
     main.run()
+    exit(0)
